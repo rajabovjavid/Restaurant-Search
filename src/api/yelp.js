f4ea0eb47a5a9fd7d3b377a3850b50a91e75005d
rajabovjavid <jvdrjbv@gmail.com>
2020-10-24 13:59:23 +0400

    using outside api
    request to yelp api

---
+++ src/api/yelp.js
@@ -0,0 +1,8 @@
+import axios from "axios"
+
+export default axios.create({
+  baseURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization: "Bearer rYqDj7EBww_03t0ZVE3HQWQ_lYfeiTIQAVWIGeyWSTqYJWkPxbhj_f4LUyYsEBrWlmrndyBIcIDZaL9yUyapidtSQ8jW8ugi9KOaywXHIGbkThxdqcnLRzPJMUqPX3Yx"
+  }
+})
---
+++ src/components/SearchBar.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { StyleSheet, View, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = ({term, onTermChange, onTermSubmit}) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        placeholderTextColor="gray"
+        value={term}
+        onChangeText = {onTermChange}
+        onEndEditing = {onTermSubmit}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 10,
+    flexDirection: "row",
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 20,
+  },
+  iconStyle: {
+    fontSize: 35,
+    marginHorizontal: 10,
+    alignSelf: "center",
+  },
+});
+
+export default SearchBar;
